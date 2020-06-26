a5ae0ac23ea84b88fa55ef43a857204fd3ac9900
Tim Schindler <xh3b4sd@gmail.com>
2020-06-26 15:49:20 +0200

    add generate command structure (#17)

---
+++ cmd/gen/ac/template/execute/flag.go
@@ -0,0 +1,17 @@
+package execute
+
+var FlagGo = `
+package execute
+
+import "github.com/spf13/cobra"
+
+type flag struct {
+}
+
+func (f *flag) Init(cmd *cobra.Command) {
+}
+
+func (f *flag) Validate() error {
+	return nil
+}
+`
---
+++ cmd/gen/ac/template/explain/runner.go
@@ -0,0 +1,61 @@
+package explain
+
+var RunnerGo = `
+package explain
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"strings"
+
+	"github.com/giantswarm/microerror"
+	"github.com/giantswarm/micrologger"
+	"github.com/spf13/cobra"
+
+	"github.com/giantswarm/awscnfm/pkg/action"
+	"github.com/giantswarm/awscnfm/pkg/action/{{ .Cluster }}/{{ .Action }}"
+)
+
+type runner struct {
+	flag   *flag
+	logger micrologger.Logger
+	stdout io.Writer
+	stderr io.Writer
+}
+
+func (r *runner) Run(cmd *cobra.Command, args []string) error {
+	ctx := context.Background()
+
+	err := r.flag.Validate()
+	if err != nil {
+		return microerror.Mask(err)
+	}
+
+	err = r.run(ctx, cmd, args)
+	if err != nil {
+		return microerror.Mask(err)
+	}
+
+	return nil
+}
+
+func (r *runner) run(ctx context.Context, cmd *cobra.Command, args []string) error {
+	var err error
+
+	var e action.Explainer
+	{
+		c := {{ .Action }}.ExplainerConfig{}
+
+		e, err = {{ .Action }}.NewExplainer(c)
+		if err != nil {
+			return microerror.Mask(err)
+		}
+	}
+
+	fmt.Println(strings.TrimSpace(e.Explain()))
+	fmt.Println()
+
+	return nil
+}
+`
