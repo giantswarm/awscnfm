84631dd7c8546044cfffd18f1e726e936ef68abe
Nick Jüttner <nick@juni.io>
2020-07-16 11:52:34 +0200

    new line

--- cmd/completion/command.go
+++ cmd/completion/command.go
@@ -13,28 +13,28 @@ const (
 	name             = "completion [bash|zsh|fish|powershell]"
 	shortDescription = "Generate completion script."
 	longDescription  = `To load completions:
-	
+
 	# Bash:
-	
+
 	$ source <(awscnfm completion bash)
-	
+
 	# To load completions for each session, execute once:
 	# Linux:
 	  $ awscnfm completion bash > /etc/bash_completion.d/awscnfm
 	# MacOS:
 	  $ awscnfm completion bash > /usr/local/etc/bash_completion.d/awscnfm
-	
+
 	# Zsh:
-	
+
 	$ source <(awscnfm completion zsh)
-	
+
 	# To load completions for each session, execute once:
 	$ awscnfm completion zsh > "${fpath[1]}/_awscnfm"
-	
+
 	# Fish:
-	
+
 	$ awscnfm completion fish | source
-	
+
 	# To load completions for each session, execute once:
 	$ awscnfm completion fish > ~/.config/fish/completions/awscnfm.fish`
 )
