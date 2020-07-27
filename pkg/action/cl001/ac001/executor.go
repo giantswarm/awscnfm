45687d47e249772717f82225079d2e3a859c52cf
Tim Schindler <xh3b4sd@gmail.com>
2020-07-27 11:07:16 +0200

    explain first action to understand which cluster is created

--- pkg/action/cl001/ac001/executor.go
+++ pkg/action/cl001/ac001/executor.go
@@ -3,43 +3,13 @@ package ac001
 import (
 	"context"
 
-	"github.com/giantswarm/apiextensions/pkg/apis/infrastructure/v1alpha2"
 	"github.com/giantswarm/microerror"
-
-	"github.com/giantswarm/awscnfm/pkg/key"
-	"github.com/giantswarm/awscnfm/pkg/release"
 )
 
 func (e *Executor) execute(ctx context.Context) error {
-	var err error
-
-	var releaseComponents map[string]string
-	{
-		c := release.Config{}
-
-		releaseCollection, err := release.New(c)
-		if err != nil {
-			return microerror.Mask(err)
-		}
-
-		releaseComponents = releaseCollection.ReleaseComponents(key.Release)
-	}
-
-	var crs v1alpha2.ClusterCRs
-	{
-		c := v1alpha2.ClusterCRsConfig{
-			Domain:            key.DomainFromHost(e.clients.ControlPlane.RESTConfig().Host),
-			Description:       explainerCommand,
-			Owner:             key.Organization,
-			Region:            key.RegionFromHost(e.clients.ControlPlane.RESTConfig().Host),
-			ReleaseComponents: releaseComponents,
-			ReleaseVersion:    key.Release,
-		}
-
-		crs, err = v1alpha2.NewClusterCRs(c)
-		if err != nil {
-			return microerror.Mask(err)
-		}
+	crs, err := newCRs(e.clients.ControlPlane.RESTConfig().Host)
+	if err != nil {
+		return microerror.Mask(err)
 	}
 
 	{
