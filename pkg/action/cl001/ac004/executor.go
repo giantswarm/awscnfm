eefb48627a0c1679b94c9e7ec5b059b03b397816
Tim Schindler <xh3b4sd@gmail.com>
2020-08-03 14:26:09 +0200

    fix linter issues

--- pkg/action/cl001/ac004/executor.go
+++ pkg/action/cl001/ac004/executor.go
@@ -3,11 +3,12 @@ package ac004
 import (
 	"context"
 
-	"github.com/giantswarm/awscnfm/pkg/label"
 	"github.com/giantswarm/microerror"
 	"k8s.io/apimachinery/pkg/api/errors"
 	apiv1alpha2 "sigs.k8s.io/cluster-api/api/v1alpha2"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+
+	"github.com/giantswarm/awscnfm/pkg/label"
 )
 
 func (e *Executor) execute(ctx context.Context) error {
