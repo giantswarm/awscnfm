e05fd06636a3bea3dee15153506aeab9269d1334
Tim Schindler <xh3b4sd@gmail.com>
2020-08-03 16:02:23 +0200

    improve plan explanation by adding time information (#60)

--- pkg/plan/step.go
+++ pkg/plan/step.go
@@ -1,15 +1,11 @@
 package plan
 
-import (
-	"github.com/giantswarm/backoff"
-)
-
 type Step struct {
 	// Action is the action/command name, e.g. ac013.
 	Action string
 	// Backoff is the tolerance within the associated action must be
 	// successfully executed without returning errors.
-	Backoff backoff.Interface
+	Backoff *Backoff
 	// Comment is only informational to provide developers hints about the
 	// purpose of a given action. This should help understanding what ac013 is
 	// doing so that one can reason about why ac027 should run next instead of
