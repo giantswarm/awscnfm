a834006cc91475fbadd644572e11c9262e273853
Tim Schindler <xh3b4sd@gmail.com>
2020-08-05 11:50:22 +0200

    check cluster access in ac002

--- pkg/action/cl001/ac002/explainer.go
+++ pkg/action/cl001/ac002/explainer.go
@@ -5,5 +5,13 @@ import (
 )
 
 func (e *Explainer) explain(ctx context.Context) (string, error) {
-	return "", nil
+	s := `
+Check if the Tenant Cluster is up and we can connect to it.
+
+	* List all Tenant Cluster nodes.
+	* Listing all Tenant Cluster nodes without errors means the apiserver is up.
+	* Proceed with further actions.
+	`
+
+	return s, nil
 }
