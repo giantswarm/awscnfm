316836902d95827a4b8e2870a343c95e280be601
Tim Schindler <xh3b4sd@gmail.com>
2020-08-05 11:28:36 +0200

    move actions around to make space for ac002 (#67)

--- pkg/action/cl001/ac005/executor.go
+++ pkg/action/cl001/ac005/executor.go
@@ -2,8 +2,28 @@ package ac005
 
 import (
 	"context"
+
+	"github.com/giantswarm/microerror"
+	"k8s.io/apimachinery/pkg/api/errors"
+	apiv1alpha2 "sigs.k8s.io/cluster-api/api/v1alpha2"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+
+	"github.com/giantswarm/awscnfm/pkg/label"
 )
 
 func (e *Executor) execute(ctx context.Context) error {
+	{
+		err := e.clients.ControlPlane.CtrlClient().DeleteAllOf(
+			ctx,
+			&apiv1alpha2.Cluster{},
+			client.MatchingLabels{label.Cluster: e.tenantCluster},
+		)
+		if errors.IsNotFound(err) {
+			// fall through
+		} else if err != nil {
+			return microerror.Mask(err)
+		}
+	}
+
 	return nil
 }
