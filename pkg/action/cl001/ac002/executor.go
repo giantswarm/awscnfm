612abf84422eb19f9d549bfc80b017b3e74578b9
Tim Schindler <xh3b4sd@gmail.com>
2020-08-05 11:57:49 +0200

    check cluster access in ac002 (#69)

--- pkg/action/cl001/ac002/executor.go
+++ pkg/action/cl001/ac002/executor.go
@@ -2,8 +2,16 @@ package ac002
 
 import (
 	"context"
+
+	"github.com/giantswarm/microerror"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 func (e *Executor) execute(ctx context.Context) error {
+	_, err := e.clients.TenantCluster.K8sClient().CoreV1().Nodes().List(metav1.ListOptions{})
+	if err != nil {
+		return microerror.Mask(err)
+	}
+
 	return nil
 }
