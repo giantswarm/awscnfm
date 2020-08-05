b7375b85e5feeb6f4f55ac97f614872a00333ec1
Tim Schindler <xh3b4sd@gmail.com>
2020-08-05 11:13:23 +0200

    generate ac005 for cl001

---
+++ cmd/cl001/ac005/execute/zz_generated.runner.go
@@ -0,0 +1,90 @@
+package execute
+
+import (
+	"context"
+	"io"
+
+	"github.com/giantswarm/microerror"
+	"github.com/giantswarm/micrologger"
+	"github.com/spf13/cobra"
+
+	"github.com/giantswarm/awscnfm/pkg/action"
+	"github.com/giantswarm/awscnfm/pkg/action/cl001/ac005"
+	"github.com/giantswarm/awscnfm/pkg/config"
+	"github.com/giantswarm/awscnfm/pkg/env"
+)
+
+type runner struct {
+	flag   *flag
+	logger micrologger.Logger
+	stdout io.Writer
+	stderr io.Writer
+}
+
+func (r *runner) Run(cmd *cobra.Command, args []string) error {
+	ctx := context.Background()
+
+	err := r.flag.Validate()
+	if err != nil {
+		return microerror.Mask(err)
+	}
+
+	err = r.run(ctx, cmd, args)
+	if err != nil {
+		return microerror.Mask(err)
+	}
+
+	return nil
+}
+
+func (r *runner) run(ctx context.Context, cmd *cobra.Command, args []string) error {
+	var err error
+
+	var clients *action.Clients
+	{
+		c := action.Config{
+			Logger: r.logger,
+
+			KubeConfig:    env.KubeConfig(),
+			TenantCluster: config.Cluster("cl001", env.TenantCluster()),
+		}
+
+		clients, err = action.NewClients(c)
+		if err != nil {
+			return microerror.Mask(err)
+		}
+
+		err = clients.InitControlPlane(ctx)
+		if err != nil {
+			return microerror.Mask(err)
+		}
+
+		err = clients.InitTenantCluster(ctx)
+		if err != nil {
+			return microerror.Mask(err)
+		}
+	}
+
+	var e action.Executor
+	{
+		c := ac005.ExecutorConfig{
+			Clients: clients,
+			Command: cmd,
+			Logger:  r.logger,
+
+			TenantCluster: config.Cluster("cl001", env.TenantCluster()),
+		}
+
+		e, err = ac005.NewExecutor(c)
+		if err != nil {
+			return microerror.Mask(err)
+		}
+	}
+
+	err = e.Execute(ctx)
+	if err != nil {
+		return microerror.Mask(err)
+	}
+
+	return nil
+}
