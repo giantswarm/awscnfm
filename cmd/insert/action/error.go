510d02e1467b7abb47d5bc9b057975d0fb147633
Tim Schindler <xh3b4sd@gmail.com>
2020-08-12 10:53:17 +0200

    add structure for insert command

---
+++ cmd/insert/action/error.go
@@ -0,0 +1,21 @@
+package action
+
+import "github.com/giantswarm/microerror"
+
+var invalidConfigError = &microerror.Error{
+	Kind: "invalidConfigError",
+}
+
+// IsInvalidConfig asserts invalidConfigError.
+func IsInvalidConfig(err error) bool {
+	return microerror.Cause(err) == invalidConfigError
+}
+
+var invalidFlagError = &microerror.Error{
+	Kind: "invalidFlagError",
+}
+
+// IsInvalidFlag asserts invalidFlagsError.
+func IsInvalidFlag(err error) bool {
+	return microerror.Cause(err) == invalidFlagError
+}
