0b1ab433a34556875ed165b0aaeded043b2f3b55
Nick Jüttner <nick@giantswarm.io>
2020-08-12 11:15:54 +0200

    Update pkg/action/cl001/ac006/explainer.go
    Co-authored-by: Tim Schindler <xh3b4sd@gmail.com>

--- pkg/action/cl001/ac006/explainer.go
+++ pkg/action/cl001/ac006/explainer.go
@@ -6,7 +6,8 @@ import (
 
 func (e *Explainer) explain(ctx context.Context) (string, error) {
 	s := `
-Check if the number of pods on master node with host network set matches the number we expect from k8scloudconfig.
+Check if the number of host network pods on tenant cluster master nodes matches the number we expect from k8scloudconfig.
+
 	* Fetch all Tenant Cluster nodes and take the the first master node by label.
 	* Compare the current pods with host network set with the expected amount of pods on master node.
 	`
