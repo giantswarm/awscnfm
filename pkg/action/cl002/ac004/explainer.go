5ff928e3dfce66bc50b108e3d725986ec3c6521e
Tim Schindler <xh3b4sd@gmail.com>
2020-09-11 15:38:12 +0200

    check cluster upgrade successfully finished

--- pkg/action/cl002/ac004/explainer.go
+++ pkg/action/cl002/ac004/explainer.go
@@ -5,5 +5,18 @@ import (
 )
 
 func (e *Explainer) explain(ctx context.Context) (string, error) {
-	return "", nil
+	s := `
+Check if the Tenant Cluster got successfully upgraded. Note that this
+particular action is not meant to be reliably used for different purposes
+than for the plan exection of cluster scope cl002. Executing this action
+against a Tenant Cluster that got already upgraded may lead to wrong results
+in case you want to assert an additional Tenant Cluster upgrade.
+
+	* Fetch the AWSCluster CR.
+	* Check if the latest cluster status condition is "Updated".
+	* Return an error if we see other cluster status conditions than "Updated".
+
+	`
+
+	return s, nil
 }
