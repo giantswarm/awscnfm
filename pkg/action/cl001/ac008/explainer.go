ba921201de513ba0c5af3ea4d74cd13a361f731f
calvix <vaclav@giantswarm.io>
2020-09-29 08:51:48 +0200

    remove aws api check job from this action

--- pkg/action/cl001/ac008/explainer.go
+++ pkg/action/cl001/ac008/explainer.go
@@ -6,12 +6,11 @@ import (
 
 func (e *Explainer) explain(ctx context.Context) (string, error) {
 	s := `
-Test Kiam app deployed on the tenant cluster. This test should do following:
+Check if Kiam's pods and secrets are present on the tenant cluster. This test should do following:
 
     * Ensure tls certs for Kiam are created.
     * Ensure kiam-server and kiam-agent pods are running without errors on the tenant cluster.
-    * Test AWS API calls are working as expected. This part will create a pod (via job) 
-      and try assume role "$cluster_id-Route53-Manager" and call "aws route53 list-domains" to ensure kiam works as expected.
+
 `
 
 	return s, nil
