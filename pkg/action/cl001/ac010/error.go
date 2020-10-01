d0bad93ad3befcace5d6612226d50448e4dca68e
calvix <vaclav@giantswarm.io>
2020-10-01 11:49:27 +0200

    add-descrintipn-for-error-job-not-completed

--- pkg/action/cl001/ac010/error.go
+++ pkg/action/cl001/ac010/error.go
@@ -5,6 +5,7 @@ import "github.com/giantswarm/microerror"
 // jobNotCompleted is an error indicating that the job in kiam test is not jet finished
 var jobNotCompleted = &microerror.Error{
 	Kind: "jobNotCompleted",
+	Desc: "AWS API call job for testing kiam is not completed",
 }
 
 // IsJobNotCompleted asserts jobNotCompleted error.
