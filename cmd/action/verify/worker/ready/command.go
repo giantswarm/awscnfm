b0cd1fb034747cbb9c6fa1061e2a46e09746d732
Tim Schindler <xh3b4sd@gmail.com>
2020-10-23 10:56:58 +0200

    add logic for action verify worker ready (#223)

--- cmd/action/verify/worker/ready/command.go
+++ cmd/action/verify/worker/ready/command.go
@@ -9,7 +9,13 @@ import (
 const (
 	name  = "ready"
 	short = "Verify if all worker nodes are ready."
-	long  = "Verify if all worker nodes are ready."
+	long  = `
+Check if the desired amount of Tenant Cluster worker nodes are up and ready.
+
+	* Fetch all AWSMachineDeployment CRs spec.scaling.min so that we know how many workers the Tenant Cluster is supposed to have.
+	* Fetch the Tenant Cluster worker nodes.
+	* Compare the current and desired amount of worker nodes.
+	`
 )
 
 type Config struct {
