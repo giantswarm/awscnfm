48525e01934b185edc220e22587172bf8372013b
Tim Schindler <xh3b4sd@gmail.com>
2020-10-29 11:24:15 +0100

    add logic for upgrading clusters

--- cmd/action/update/cluster/patch/flag.go
+++ cmd/action/update/cluster/patch/flag.go
@@ -1,13 +1,24 @@
 package patch
 
-import "github.com/spf13/cobra"
+import (
+	"github.com/giantswarm/microerror"
+	"github.com/spf13/cobra"
+
+	"github.com/giantswarm/awscnfm/v12/pkg/env"
+)
 
 type flag struct {
+	TenantCluster string
 }
 
 func (f *flag) Init(cmd *cobra.Command) {
+	cmd.Flags().StringVarP(&f.TenantCluster, "tenant-cluster", "c", env.TenantCluster(), "Tenant Cluster ID to use for this particular action.")
 }
 
 func (f *flag) Validate() error {
+	if f.TenantCluster == "" {
+		return microerror.Maskf(invalidFlagsError, "-c/--tenant-cluster must not be empty")
+	}
+
 	return nil
 }
