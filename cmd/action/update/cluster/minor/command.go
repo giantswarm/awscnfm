2555d201f8168162c090e851940a6ff40da6d92e
Tim Schindler <xh3b4sd@gmail.com>
2020-10-29 10:32:18 +0100

    add structure for update commands (#242)

---
+++ cmd/action/update/cluster/minor/command.go
@@ -0,0 +1,40 @@
+package minor
+
+import (
+	"github.com/giantswarm/microerror"
+	"github.com/giantswarm/micrologger"
+	"github.com/spf13/cobra"
+)
+
+const (
+	name        = "minor"
+	description = "Update a Tenant Cluster to the latest minor release."
+)
+
+type Config struct {
+	Logger micrologger.Logger
+}
+
+func New(config Config) (*cobra.Command, error) {
+	if config.Logger == nil {
+		return nil, microerror.Maskf(invalidConfigError, "%T.Logger must not be empty", config)
+	}
+
+	f := &flag{}
+
+	r := &runner{
+		flag:   f,
+		logger: config.Logger,
+	}
+
+	c := &cobra.Command{
+		Use:   name,
+		Short: description,
+		Long:  description,
+		RunE:  r.Run,
+	}
+
+	f.Init(c)
+
+	return c, nil
+}
