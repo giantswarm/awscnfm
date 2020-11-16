1a3fed0a2e3bc0196a4e8f9300444fd0a6279d2f
calvix <vaclav@giantswarm.io>
2020-11-16 15:45:58 +0100

    action-verify-netpol-curlrequest

---
+++ cmd/action/verify/netpol/curlrequest/command.go
@@ -0,0 +1,43 @@
+package curlrequest
+
+import (
+	"github.com/giantswarm/microerror"
+	"github.com/giantswarm/micrologger"
+	"github.com/spf13/cobra"
+)
+
+const (
+	name  = "curlrequest"
+	short = "Verify that the curl test request jobs were executed as expected."
+	long  = `Verify that the curl test request jobs were executed as expected. 
+Job inside of 'test' namespace should succeed and Job inside 'default' namespace should fail.
+`
+)
+
+type Config struct {
+	Logger micrologger.Logger
+}
+
+func New(config Config) (*cobra.Command, error) {
+	if config.Logger == nil {
+		return nil, microerror.Maskf(invalidConfigError, "%T.Logger must not be empty", config)
+	}
+
+	f := &flag{}
+
+	r := &runner{
+		flag:   f,
+		logger: config.Logger,
+	}
+
+	c := &cobra.Command{
+		Use:   name,
+		Short: short,
+		Long:  long,
+		RunE:  r.Run,
+	}
+
+	f.Init(c)
+
+	return c, nil
+}
