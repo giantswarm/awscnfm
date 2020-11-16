5aa4cd99d8fd229d3c54477ac62318621a0f9175
calvix <vaclav@giantswarm.io>
2020-11-16 11:18:52 +0100

    action-create-netpol-defaultnetpol

--- pkg/key/key.go
+++ pkg/key/key.go
@@ -1,8 +1,14 @@
 package key
 
 import (
+	"context"
 	"fmt"
 	"strings"
+
+	"github.com/giantswarm/microerror"
+	valuemodifierpath "github.com/giantswarm/valuemodifier/path"
+	apiv1 "k8s.io/api/core/v1"
+	k8sruntimeclient "sigs.k8s.io/controller-runtime/pkg/client"
 
 	"github.com/giantswarm/awscnfm/v12/pkg/project"
 )
@@ -18,6 +24,26 @@ const (
 
 const (
 	KiamTestNamespace = "kube-system"
+)
+
+const (
+	draughtsmanNamespace                  = "draughtsman"
+	draughtsmanConfigMapName              = "draughtsman-values-configmap"
+	draughtsmanConfigMapDataKey           = "values"
+	draughtsmanConfigMapDockerRegistryKey = "Installation.V1.Registry.Domain"
+)
+
+const (
+	NetPolDefaultNamespaceName = "default"
+	NetPolTestNamespaceName    = "test"
+
+	NetPolName                 = "deny-from-all-namespaces"
+	NetPolNginxTestPodName     = "netpol-nginx-test-pod"
+	NetPolNginxTestPodAppLabel = "nginx-test-pod"
+	NetPolNginxSvcName         = "netpol-nginx-test-svc"
+
+	LabelApp       = "app"
+	LabelManagedBy = "managed-by"
 )
 
 func APIEndpoint(id string, base string) string {
@@ -39,6 +65,14 @@ func KiamTestNetPolName() string {
 	return fmt.Sprintf("%s-kiam-test", project.Name())
 }
 
+func NetPolCurlTestCommand() string {
+	return fmt.Sprintf("/usr/bin/curl --connect-timeout 5 %s.%s.svc", NetPolNginxSvcName, NetPolTestNamespaceName)
+}
+
+func NetPolTestJobName(namespace string) string {
+	return fmt.Sprintf("%s-netpol-%s-job", project.Name(), namespace)
+}
+
 func RegionFromHost(h string) string {
 	h = strings.Split(DomainFromHost(h), ".")[1]
 	// Domain in giraffe is giraffe.pek.aws.k8s.adidas.com.cn which does not follow the convention
@@ -46,4 +80,45 @@ func RegionFromHost(h string) string {
 		h = "cn-north-1"
 	}
 	return h
+}
+
+func FetchDockerRegistry(ctx context.Context, cpCtrlClient k8sruntimeclient.Client) (string, error) {
+	var dockerRegistry string
+
+	cm := &apiv1.ConfigMap{}
+
+	err := cpCtrlClient.Get(
+		ctx,
+		k8sruntimeclient.ObjectKey{
+			Name:      draughtsmanConfigMapName,
+			Namespace: draughtsmanNamespace,
+		},
+		cm)
+
+	if err != nil {
+		return "", microerror.Mask(err)
+	}
+
+	var valueReaderService *valuemodifierpath.Service
+	{
+		valueReaderConfig := valuemodifierpath.DefaultConfig()
+		valueReaderConfig.InputBytes = []byte(cm.Data[draughtsmanConfigMapDataKey])
+		valueReaderService, err = valuemodifierpath.New(valueReaderConfig)
+		if err != nil {
+			return "", microerror.Mask(err)
+		}
+
+		value, err := valueReaderService.Get(draughtsmanConfigMapDockerRegistryKey)
+		if err != nil {
+			return "", microerror.Mask(err)
+		}
+
+		var ok bool
+		dockerRegistry, ok = value.(string)
+		if !ok {
+			return "", microerror.Maskf(executionFailedError, "Failed to parse DockerRegistry value from draughtsman configmap on CP.")
+		}
+	}
+
+	return dockerRegistry, nil
 }
