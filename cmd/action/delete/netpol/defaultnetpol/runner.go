ebd04ec54cabcbc86eb7f22780881991ac971470
calvix <vaclav@giantswarm.io>
2020-11-17 11:08:46 +0100

    Action delete netpol defaultnetpol (#275)

---
+++ cmd/action/delete/netpol/defaultnetpol/runner.go
@@ -0,0 +1,98 @@
+package defaultnetpol
+
+import (
+	"context"
+
+	"github.com/giantswarm/k8sclient/v4/pkg/k8sclient"
+	"github.com/giantswarm/microerror"
+	"github.com/giantswarm/micrologger"
+	"github.com/spf13/cobra"
+	apiv1 "k8s.io/api/core/v1"
+	apismetav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	k8sruntimeclient "sigs.k8s.io/controller-runtime/pkg/client"
+
+	"github.com/giantswarm/awscnfm/v12/pkg/client"
+	"github.com/giantswarm/awscnfm/v12/pkg/env"
+	"github.com/giantswarm/awscnfm/v12/pkg/key"
+)
+
+type runner struct {
+	flag   *flag
+	logger micrologger.Logger
+}
+
+func (r *runner) Run(cmd *cobra.Command, args []string) error {
+	ctx := context.Background()
+
+	err := r.flag.Validate()
+	if err != nil {
+		return microerror.Mask(err)
+	}
+
+	err = r.run(ctx, cmd, args)
+	if err != nil {
+		return microerror.Mask(err)
+	}
+
+	return nil
+}
+
+func (r *runner) run(ctx context.Context, cmd *cobra.Command, args []string) error {
+	var err error
+
+	var cpClients k8sclient.Interface
+	{
+		c := client.ControlPlaneConfig{
+			Logger: r.logger,
+
+			KubeConfig: env.ControlPlaneKubeConfig(),
+		}
+
+		cpClients, err = client.NewControlPlane(c)
+		if err != nil {
+			return microerror.Mask(err)
+		}
+	}
+
+	var tcClients k8sclient.Interface
+	{
+		c := client.TenantClusterConfig{
+			ControlPlane: cpClients,
+			Logger:       r.logger,
+
+			TenantCluster: r.flag.TenantCluster,
+		}
+
+		tcClients, err = client.NewTenantCluster(c)
+		if err != nil {
+			return microerror.Mask(err)
+		}
+	}
+
+	err = r.cleanupNamespace(ctx, tcClients.CtrlClient())
+	if err != nil {
+		return microerror.Mask(err)
+	}
+
+	return nil
+}
+
+// cleanupNamespace will delete namespace 'test'
+// cleaning namespace is enought as kubernetes will automatically clean all resources in the namespace before deleting it
+func (r *runner) cleanupNamespace(ctx context.Context, tcClient k8sruntimeclient.Client) error {
+	ns := &apiv1.Namespace{
+		TypeMeta: apismetav1.TypeMeta{
+			Kind:       "Namespace",
+			APIVersion: apiv1.GroupName,
+		},
+		ObjectMeta: apismetav1.ObjectMeta{
+			Name: key.NetPolTestNamespaceName,
+		},
+	}
+
+	err := tcClient.Delete(ctx, ns)
+	if err != nil {
+		return microerror.Mask(err)
+	}
+	return nil
+}
