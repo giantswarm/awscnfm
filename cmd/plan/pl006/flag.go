b46b7e8162502452beb8e91f94d39f4d9cda2fa0
Tim Schindler <xh3b4sd@gmail.com>
2021-01-08 14:13:38 +0100

    consider cluster id from env when executing plan (#304)

--- cmd/plan/pl006/flag.go
+++ cmd/plan/pl006/flag.go
@@ -2,14 +2,23 @@ package pl006
 
 import (
 	"github.com/spf13/cobra"
+
+	"github.com/giantswarm/awscnfm/v12/pkg/env"
+	"github.com/giantswarm/awscnfm/v12/pkg/generate"
 )
 
 type flag struct {
+	TenantCluster string
 }
 
 func (f *flag) Init(cmd *cobra.Command) {
+	cmd.Flags().StringVarP(&f.TenantCluster, "tenant-cluster", "c", env.TenantCluster(), "Tenant Cluster ID to use for this particular action.")
 }
 
 func (f *flag) Validate() error {
+	if f.TenantCluster == "" {
+		f.TenantCluster = generate.ID()
+	}
+
 	return nil
 }
