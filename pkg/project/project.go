65617ac65924a830150b204635e37322cf98cb34
GitHub Action <action@github.com>
2021-07-16 15:08:31 +0000

    Bump version to 15.0.6-dev

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -10,7 +10,7 @@ var (
 	// conformance testing. Please make sure that at least major and minor
 	// levels of this version aligns with each new release so that we stay
 	// synchronized with the latest AWS release we publish.
-	version = "15.0.5"
+	version = "15.0.6-dev"
 )
 
 func Description() string {
