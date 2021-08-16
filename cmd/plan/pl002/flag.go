367cb030bbe1b6354087fef6233fd46162d87c70
paurosello <pau@giantswarm.io>
2021-08-16 10:05:36 +0200

    Add output to all plans (#432)
    * Add output to all plans
    
    * Add changelog
    
    * fix imports

--- cmd/plan/pl002/flag.go
+++ cmd/plan/pl002/flag.go
@@ -1,7 +1,12 @@
 package pl002
 
 import (
+	"io/ioutil"
+	"path/filepath"
+
 	"github.com/spf13/cobra"
+
+	"github.com/giantswarm/microerror"
 
 	"github.com/giantswarm/awscnfm/v15/pkg/env"
 	"github.com/giantswarm/awscnfm/v15/pkg/generate"
@@ -9,15 +14,26 @@ import (
 
 type flag struct {
 	TenantCluster string
+	Output        string
 }
 
 func (f *flag) Init(cmd *cobra.Command) {
 	cmd.Flags().StringVarP(&f.TenantCluster, "tenant-cluster", "c", env.TenantCluster(), "Tenant Cluster ID to use for this particular action.")
+	cmd.Flags().StringVar(&f.Output, "output", "", `The directory in which to store the cluster ID, kubeconfig, and provider of the created cluster.`)
 }
 
 func (f *flag) Validate() error {
 	if f.TenantCluster == "" {
 		f.TenantCluster = generate.ID()
+	}
+
+	// Write cluster ID to filesystem
+	{
+		clusterIDPath := filepath.Join(f.Output, "cluster-id")
+		err := ioutil.WriteFile(clusterIDPath, []byte(f.TenantCluster), 0644) //#nosec
+		if err != nil {
+			return microerror.Mask(err)
+		}
 	}
 
 	return nil
